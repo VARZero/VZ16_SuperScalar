18facd2186cfee9734b533832757806fddbe5881
VARZero <cydiazero@gmail.com>
2024-03-31 01:18:20 +0900

    내가이거시간될때우선순위디코더까지무조건만들고만다

--- RTL/Front-End/decoder.sv
+++ RTL/Front-End/decoder.sv
@@ -5,15 +5,19 @@
 module vz16_decoder(oneInst, inPC, outMicroOp, outPC, outRn, outR1, outR2);
     input [15:0] oneInst; // input one Instrucion
     input [15:0] inPC; // input Instruction Address
-    output logic [9:0] outMicroOp; // output Micro Opcode (IMM / Branch / MEM / STACK / SHIFT / ALU / MUL / LOGIC / )
+    output logic [7:0] outMicroOp; // output Micro Opcode (IMM / Branch / MEM / STACK / SHIFT / ALU / MUL / LOGIC / )
     output [15:0] outPC; // output Instruction Address (Program Counter)
     output [3:0] outRn, outR1, outR2; // parsing Register Fields
 
-    always_comb begin
+    always_comb begin // decoding
         casex(oneInst[3:0])
-            4'b0000: begin outMicroOp = 10'b00_0000_0001 end
-            4'b0001: begin outMicroOp = 10'b00_0000_0010 end
-            4'b001x: begin outMicroOp = {oneInst[], 8'b0000_0100} end
+            4'b0000: begin outMicroOp = 8'b0000_0001 end // IMM
+            4'b0001: begin outMicroOp = 8'b0000_0010 end // JUMP
+            4'b001x: begin outMicroOp = {3'b000, oneInst[0], 4'b0100} end // LOAD/SAVE
+            4'b001x: begin outMicroOp = {3'b001, oneInst[0], 4'b0100} end // PUSH/POP
+            4'b011x: begin outMicroOp = {3'b100, oneInst[0], 4'b1000} end // SHIFTER (ALU)
+            4'b1xxx: begin outMicroOp = {1'b0, oneInst[2:0], 4'b1000} end // ALU
+            default: begin outMicroOp = 8'bx; end
         endcase
     end
 
--- RTL/Front-End/renamer.v
+++ RTL/Front-End/renamer.v
@@ -5,5 +5,9 @@
 module scRenamer();
     input clk, reset_n;
     input [15:0] i0, i1, i2, i3;
-    output 
+    input [15:0] pc0, pc1, pc2, pc3;
+    output reg [15:0] oi0, oi1, oi2, oi3;
+    output reg RN_ACTIVE;
+
+    RegisterFile #(.ENTRY(32), .WIDTH(16), .RCha(4), .WCha(4)) U0_tempRegList()
 endmodule
